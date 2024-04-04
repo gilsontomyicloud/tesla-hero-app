ace46ee230c19a64f709073a4b08d4dbabcc71bb
Gilson <gilsontomy@icloud.com>
2024-04-04 11:31:49 +0530

    Changes to responsive design in gallery and seeder limited to 2

--- api/database/seeders/VehiclesSeeder.php
+++ api/database/seeders/VehiclesSeeder.php
@@ -39,30 +39,7 @@ class VehiclesSeeder extends Seeder
                 'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
                 'updated_at' => Carbon::now()->format('Y-m-d H:i:s')
             ],
-            [
-                'user_id' => 1,
-                'name' => 'Model X',
-                'slug' => 'model-x',
-                'short_description' => 'With ample storage and 5,000 lbs of towing capacity, Model X is built for maximum utility.',
-                'description' => 'With the most power and quickest acceleration of any SUV, Model X Plaid is the highest performing SUV ever built. Updated battery architecture enables both Long Range and Plaid configurations to complete back-to-back track runs without performance degradation.',
-                'main_image' => null,
-                'thumb_image' => null,
-                'status' => 1,
-                'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
-                'updated_at' => Carbon::now()->format('Y-m-d H:i:s')
-            ],
-            [
-                'user_id' => 1,
-                'name' => 'Model Y',
-                'slug' => 'model-y',
-                'short_description' => 'From daily driving to family road trips, charging Model Y is fast, convenient and accessible anywhere there\'s electricity.',
-                'description' => 'Go ahead, take the road trip. With up to 310 miles (EPA est.) of range on a single charge, chances are you\'ll need a break before your Model Y will. Dual motor all-wheel drive ensures you\'re ready to tackle any road conditions.',
-                'main_image' => null,
-                'thumb_image' => null,
-                'status' => 1,
-                'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
-                'updated_at' => Carbon::now()->format('Y-m-d H:i:s')
-            ],
+            
 
         ]);
     }
